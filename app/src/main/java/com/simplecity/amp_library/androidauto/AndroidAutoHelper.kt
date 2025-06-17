ca634eb78b316600547184cd620b3d8eccb05d14
YohDev24 <yohan.cas.pro@gmail.com>
2025-06-17 14:33:06 +0200

    correction caste

--- app/src/main/java/com/simplecity/amp_library/androidauto/AndroidAutoHelper.kt
+++ app/src/main/java/com/simplecity/amp_library/androidauto/AndroidAutoHelper.kt
@@ -153,7 +153,9 @@ class MediaIdHelper(
         val mediaWrapper = parseMediaId(mediaId)
         when (mediaWrapper) {
             is MediaIdWrapper.Song -> {
-                getSongsForPredicate { if (mediaWrapper.albumId == null) true else it.albumId == mediaWrapper.albumId }
+                getSongsForPredicate {
+                    mediaWrapper.albumId == null || it.albumId == mediaWrapper.albumId
+                }
                     .map { songs ->
                         songs
                             .sortedBy { song -> song.albumArtistName }
@@ -311,7 +313,7 @@ class MediaIdHelper(
 
     @SuppressLint("CheckResult")
     private fun listSongs(mediaId: String, albumId: Long?, completion: (MutableList<MediaItem>) -> Unit) {
-        getSongsForPredicate { if (albumId == null) true else it.albumId == albumId }
+    getSongsForPredicate { albumId == null || it.albumId == albumId }
             .map { songs ->
                 songs
                     .sortedBy { song -> song.albumArtistName }
